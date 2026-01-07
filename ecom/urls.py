511b963e4212f617d26bb84d443881ae8eac4154
rm0495000-afk <rm0495000@gmail.com>
2026-01-08 00:45:38 +0530

    Fix root URL and add auth/static pages

--- ecom/urls.py
+++ ecom/urls.py
@@ -5,9 +5,8 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('amazon/', include('amazon.urls')),
+    path('', include('amazon.urls')),   # 🔥 ROOT HOME
 ]
 
-# 🔥 THIS LINE IS MUST FOR IMAGE
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
