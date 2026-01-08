3f045771ce927e85d908d3ff82a046f728599754
rm0495000-afk <rm0495000@gmail.com>
2026-01-08 14:50:15 +0530

    Auto create admin user on Render startapp

--- manage.py
+++ manage.py
@@ -11,3 +11,17 @@ def main():
 
 if __name__ == '__main__':
     main()
+
+if os.environ.get("RENDER"):
+    try:
+        from django.contrib.auth import get_user_model
+        User = get_user_model()
+
+        if not User.objects.filter(username="admin").exists():
+            User.objects.create_superuser(
+                username="admin",
+                email="admin@example.com",
+                password="admin123"
+            )
+    except Exception as e:
+        pass
